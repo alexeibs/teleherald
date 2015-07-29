5187a90b7651f9e73870ddbbd38043c353ed6f5a
Alexei Bykov <alexei4203@yandex.ru>
2015-07-30 01:58:04 +0300

    Save output files into build directory

--- run-tests.bat
+++ run-tests.bat
@@ -5,7 +5,7 @@ dub build -b unittest -c unittests
 if ERRORLEVEL 1 goto build_error
 
 echo --- Run tests without coverage ---
-tests.exe
+build\tests.exe
 if ERRORLEVEL 1 goto tests_failed
 
 echo --- Run tests with coverage ---
@@ -13,7 +13,7 @@ if exist "%~dp0coverage" (
   rmdir "%~dp0coverage" /s /q
 )
 
-opencppcoverage --sources "%~dp0source" --export_type "html:%~dp0coverage" tests.exe
+opencppcoverage --sources "%~dp0source" --export_type "html:%~dp0coverage" build\tests.exe
 
 echo --- Finished ---
 exit /b 0
