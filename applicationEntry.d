32c0a9ad00868d74d7bd12a108da21b7a0feb6fb
Alexei Bykov <alexei4203@yandex.ru>
2015-08-04 01:20:31 +0300

    basic web server

--- applicationEntry.d
+++ applicationEntry.d
@@ -1,8 +1,45 @@
+import std.conv;
+import std.file;
 import std.stdio;
+import std.string;
+
+import vibe.d;
+import vibe.vibe;
+
+import activation_list;
 import app;
+import chat_collection;
+import common_types;
+import config;
+
+void showCode(HTTPServerRequest request, HTTPServerResponse response) {
+  response.render!("activation.dt", request);
+}
+
+shared static this() {
+  string json;
+  try {
+    json = readText("teleherald.json");
+  } catch(Exception e) {
+    writeln("Cannot read config file. Error: ", e.msg);
+  }
+  auto config = parseConfig(json);
+
+  auto router = new URLRouter;
+  router.get("/" ~ config.activatorPath(), &showCode);
+
+  auto settings = new HTTPServerSettings;
+  settings.port = 8080;
+  listenHTTP(settings, router);
+}
 
 class RealApp : App {
-  void start() {}
+  void start() {
+    if (!finalizeCommandLineOptions())
+      return;
+    lowerPrivileges();
+    runEventLoop();
+  }
 }
 
 void main()
