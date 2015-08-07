2d90805c17c816b0c4bdfab7daebe6482e323438
Alexei Bykov <alexei4203@yandex.ru>
2015-08-07 23:31:02 +0300

    [D][JS] fixed test.html entry point

--- client/scripts/entry-point.js
+++ client/scripts/entry-point.js
@@ -1,10 +1,10 @@
 define(['scripts/server', 'scripts/data-model', 'scripts/activation-view.jsx'],
   function(Server, DataModel, ActivationView) {
     return {
-      run: function(contentPanel) {
-        var server = Server.create(XMLHttpRequest, location.pathname);
+      run: function(params) {
+        var server = Server.create(XMLHttpRequest, params.requestBasePath);
         var model = DataModel.create(server, 2000);
-        var view = ActivationView.create(contentPanel, model.addNewChat.bind(model));
+        var view = ActivationView.create(params.viewContainer, model.addNewChat.bind(model));
         model.addActivationView(view);
       }
     };
