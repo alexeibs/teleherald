0214316e969eaa65a882f2f9b0838e50e547e3c8
Alexei Bykov <alexei4203@yandex.ru>
2015-08-07 03:42:58 +0300

    [D] Implement REST interface for ActivationList

--- applicationEntry.d
+++ applicationEntry.d
@@ -3,8 +3,13 @@ import std.file;
 import std.stdio;
 import std.string;
 
-import vibe.d;
-import vibe.vibe;
+import vibe.core.args;
+import vibe.core.concurrency;
+import vibe.core.core;
+import vibe.core.log;
+import vibe.http.router;
+import vibe.http.server;
+import vibe.web.rest;
 
 import activation_list;
 import app;
@@ -12,11 +17,43 @@ import chat_collection;
 import common_types;
 import config;
 
+__gshared Task g_keeperTask;
+
+void runKeeperTask()
+{
+  auto activationList = createActivationList(null);
+  g_keeperTask = runTask({
+    while (true) {
+      receive(
+        (Task target) {
+          target.send(activationList.getActivationList());
+        },
+        (Task target, string chatName) {
+          target.send(activationList.postActivationList(chatName));
+        });
+    }
+  });
+  runEventLoop();
+}
+
+class ActivationRestInterfaceImpl : ActivationRestInterface {
+  ActivationEntries getActivationList() {
+    g_keeperTask.send(Task.getThis());
+    return receiveOnly!ActivationEntries;
+  }
+  ActivationEntry postActivationList(string chatName) {
+    g_keeperTask.send(Task.getThis(), chatName);
+    return receiveOnly!ActivationEntry;
+  }
+}
+
 void showCode(HTTPServerRequest request, HTTPServerResponse response) {
   response.render!("activation.dt", request);
 }
 
 shared static this() {
+  new core.thread.Thread(&runKeeperTask).start();
+
   string json;
   try {
     json = readText("teleherald.json");
@@ -27,6 +64,8 @@ shared static this() {
 
   auto router = new URLRouter;
   router.get("/" ~ config.activatorPath(), &showCode);
+  router.registerRestInterface(new ActivationRestInterfaceImpl,
+      "/" ~ config.activatorPath() ~ "/", MethodStyle.camelCase);
 
   auto settings = new HTTPServerSettings;
   settings.port = 8080;
